823f4498cf28e701e5cf953e2c1cd1f70f291fd6
Alice Post <alicep2322@gmail.com>
2020-01-29 17:36:08 -0700

    Find user with given api_key

--- routes/api/v1/forecast.js
+++ routes/api/v1/forecast.js
@@ -5,12 +5,48 @@ const environment = process.env.NODE_ENV || 'development';
 const configuration = require('../../../knexfile')[environment];
 const database = require('knex')(configuration);
 
+var googleGeoService = require('../../services/googleGeoService');
 
 router.get('/', (request, response) => {
   // 0. Check api_key in body to make sure it's in db
-  key = request.body
-  response.send(key)
+  const user = request.body;
+
+  for (let requiredParameter of ['api_key']) {
+    if (!user[requiredParameter]) {
+      return response
+        .status(422)
+        .send({ error: `Expected format: { api_key: <String> }. You're missing a "${requiredParameter}" property.` });
+    }
+  }
+
+  database('users')
+    .where('api_key', user['api_key'])
+    .select()
+    .then(users => {
+      if(users.length === 0) {
+        return response
+        .status(404)
+        .send({error: "Unauthorized Request"});
+      } else {
+        // authorized request made!
+        let foundUser = users[0]
+        latLong = googleGeoService(request.query)
+        console.log(latLong)
+        return response
+        .status(200)
+        .send({success: "Authorized Request"})
+      }
+    })
+    .catch(error => {
+      response.status(500).json({error})
+    })
+    .then(foundUser => console.log(foundUser))
+    // response.send(foundUser)
+
+  // response.send('foundUser')
+
   // 1. get parameters from request
+  let searchLocation = request.query
   // 2. Send parameters to geocode serivce to get lat and long
   // 3. Send lat and long to darksky service to get forecast
   // 4. format forecast into expected json format
