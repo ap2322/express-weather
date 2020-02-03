2b2a640d50fdc8cd2640be9644547fead76088ae
Alice Post <alicep2322@gmail.com>
2020-02-02 23:20:11 -0700

    Remove papers routes from app.js

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
 
 var indexRouter = require('./routes/index');
-var papersRouter = require('./routes/api/v1/papers');
 var forecastRouter = require('./routes/api/v1/forecast');
 var favoritesRouter = require('./routes/api/v1/favorites');
 
@@ -21,7 +20,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/api/v1/papers', papersRouter);
 app.use('/api/v1/forecast', forecastRouter);
 app.use('/api/v1/favorites', favoritesRouter);
 
